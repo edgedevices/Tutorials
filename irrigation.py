44eca712cee362a5207ddf3048efffdb285feff2
Nate Albright <52329099+natealbright@users.noreply.github.com>
2021-11-13 14:23:55 -0500

    Update irrigation.py

--- irrigation.py
+++ irrigation.py
@@ -4,12 +4,12 @@ from gpiozero import MCP3208
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.OUT)
-GPIO.setup(19, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT) #Digital Output #1
+GPIO.setup(20, GPIO.OUT) #Digital Output #2
+GPIO.setup(21, GPIO.OUT) #Digital Output #3
+GPIO.setup(26, GPIO.OUT) #Digital Output #4
+GPIO.setup(19, GPIO.OUT) #Digital Output #5
+GPIO.setup(13, GPIO.OUT) #Digital Output #6
 
 adc0 = MCP3208(channel = 0)
 
@@ -18,9 +18,9 @@ while True:
 		moisture = adc0.value
 		print(moisture)
 		print('watering')
-		GPIO.output(13, True) #turn pump on
+		GPIO.output(16, True) #turn pump on
 		sleep(2) #water length
-		GPIO.output(13, False) #turn pump off
+		GPIO.output(16, False) #turn pump off
 		print('watered')
 		sleep(2)
 	else:
